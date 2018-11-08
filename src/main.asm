387ad63f59c8da722040d8532c640eef875ddd1a
2010kohtep <sania40000@yandex.ru>
2018-11-08 16:53:43 +0300

    add .bss section

--- src/main.asm
+++ src/main.asm
@@ -10,6 +10,7 @@ include 'win32a.inc'
 include 'macro\proc32.inc'
 
 include 'sec_text.asm'
+include 'sec_bss.asm'
 include 'sec_const.asm'
 include 'sec_data.asm'
 include 'sec_idata.asm'
---
+++ src/sec_bss.asm
@@ -0,0 +1,10 @@
+;
+; —екци€ неинициализированных данных
+;
+
+section '.bss' data readable writable
+
+  ; ќбъ€вление массива структур с помощью db, так как средства €зыка FASM
+  ; не позвол€ют сделать иначе.
+  gChunks    db sizeof.drawchunk_t * 4096 dup(?)
+  gLastChunk drawchunk_t ? ; ѕеременна€ дл€ обозначени€ конца массива gChunks
